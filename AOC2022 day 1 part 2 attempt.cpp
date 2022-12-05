48155258eb90f51883138f45b1aba3eae2e0017a
ken01234 <119032685+ken01234@users.noreply.github.com>
2022-12-05 11:46:26 +0800

    Add files via upload

---
+++ AOC2022 day 1 part 2 attempt.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string myText;
+    int sum = 0;
+    ifstream MyReadFile("input.txt");
+    vector<int> data={};
+    long long i = 0;
+    while (getline (MyReadFile, myText)){
+          if (myText.empty()){
+        data.push_back(sum);
+        sum =0;
+        continue;
+            }
+      long long calories = stoi(myText);
+      sum = sum + calories;
+      }
+    sort(data.begin(),data.end());
+  cout << *max_element(data.begin(),data.end()) <<endl << data[data.size()-1] << endl << data[data.size()-2] << endl;
+  int answer = *max_element(data.begin(),data.end()) + data[data.size()-2] + data[data.size()-3];
+   cout << answer;
+  
+}
