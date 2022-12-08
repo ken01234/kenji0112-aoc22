e65c5d966fc0e715d99634f507130bae64ba8f7a
ken01234 <119032685+ken01234@users.noreply.github.com>
2022-12-08 21:55:06 +0800

    Add files via upload

---
+++ aoc2022_day4_attempt.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+int main() {
+    string myText;
+    ifstream MyReadFile("aoc2022day4input.txt");
+  vector<string> sector= {};
+  while (getline (MyReadFile, myText)){
+    sector.push_back(myText);
+    cout<<myText<<endl;
+    }
+  char delimiter1 = ',';
+  char delimiter2 = '-';
+
+  int count = 0;
+
+  for (const string& s : sector) {
+    stringstream ss(s);
+    vector<string> leftover;
+    string left;
+    while (getline(ss, left, delimiter1)) {
+      leftover.push_back(left);
+    }
+
+    vector<string> range1, range2;
+    stringstream ss1(leftover[0]);
+    while (getline(ss1, left, delimiter2)) {
+      range1.push_back(left);
+    }
+    stringstream ss2(leftover[1]);
+    while (getline(ss2, left, delimiter2)) {
+      range2.push_back(left);
+    }
+
+    int start1 = stoi(range1[0]);
+    int end1 = stoi(range1[1]);
+    int start2 = stoi(range2[0]);
+    int end2 = stoi(range2[1]);
+
+
+    if ((end2 > end1 && start2 > start1)||(end1 > end2 && start1 < start2)||(end2 == end1 && start2 > start1)||(end2 > end1 && start2 == start1)||(end1 ==end2 && start1 < start2)||(end1 > end2 && start1 == start2)){
+      count++;
+    }
+  }
+
+  cout << count;
+
+  return 0;
+}
