637cad6971e92abc5271e65ce38bf0b0b518f343
ken01234 <119032685+ken01234@users.noreply.github.com>
2022-12-12 00:07:26 +0800

    Add files via upload

---
+++ AOC 2022 day 5 attempt.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+// FPHFQPQWT
+int main() {
+  int count=0;
+  vector<vector<string>> crates ={ {"D", "L", "V", "T", "M", "H", "F"}, {"H", "Q", "G", "J", "C", "T", "N","P"},{"R", "S", "D", "M", "P", "H"},{"L", "B", "V", "F"},{"N", "H", "G", "L", "Q"},{"W", "B", "D", "G", "R", "M", "P"}, {"G", "M", "N", "R", "C", "H", "L", "Q"}, {"C", "L", "W"}, {"R", "D", "L", "Q", "J", "Z", "M", "T"}};
+
+string myText;
+vector<string> tokens;
+  ifstream MyReadFile("AOCday5input.txt");
+  while (getline(MyReadFile, myText)) {
+    count+=1;
+    cout<<endl<<count<<endl;
+    stringstream ss(myText);
+    while (getline(ss, myText, ' ')) {
+      tokens.push_back(myText);
+      }
+  
+  int n1 = stoi(tokens[1]);
+  int from = (stoi(tokens[3])) - 1;
+  int to = (stoi(tokens[5])) - 1;
+     for (int i = 0; i < n1; i++) {
+    if (crates[from].size() == 0) {
+         break;
+      }else{
+     crates[to].push_back(crates[from][crates[from].size()-1]);
+      cout<<crates[from][crates[from].size()-1]<<endl;
+       crates[from].pop_back();
+       }
+       } 
+    
+   vector<string> longestVec;
+   for (const auto &innerVec : crates) {
+     if (innerVec.size() > longestVec.size()) {
+       longestVec = innerVec;
+     }
+   }
+   for (auto elem : crates) {
+     if (elem.size()==0){
+       cout<<"";
+     }else{
+     cout << elem[elem.size()-1];
+       }
+   }
+ }
+  }
